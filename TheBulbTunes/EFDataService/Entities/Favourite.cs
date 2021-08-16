9699bb77b84c327f423386b09fa63a74dfcb9927
Adeleke Ayinde <lekea4@gmail.com>
2021-08-16 17:49:22 +0100

    Define Favourit entity

--- TheBulbTunes/EFDataService/Entities/Favourite.cs
+++ TheBulbTunes/EFDataService/Entities/Favourite.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TheBulbTunes.EFDataService.Entities
 {
-    class Favourite
+    public class Favourite
     {
+        [Required]
+        public Guid ID { get; set; }
+
+        [Required]
+        public Guid SelectedSongID { get; set; }
+
+        [Required]
+        public Guid AddedByID { get; set; }
+
+        [Required]
+
+        public DateTime DateAdded { get; set; }
+
+
+
+        
+        //The following are navigation properties made possible by the foreign-key relationships 
+        public virtual Song SelectedSong { get; set; }
+
+        public virtual User AddedBy  { get; set; }
+
+
+
     }
 }
