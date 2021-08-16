14e6dcb65b31fa3a18e1a6b6171bb97cbbb99bca
Adeleke Ayinde <lekea4@gmail.com>
2021-08-16 17:48:24 +0100

    Added favouriteList as a navigation property

--- TheBulbTunes/EFDataService/Entities/Song.cs
+++ TheBulbTunes/EFDataService/Entities/Song.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TheBulbTunes.EFDataService.Entities
 {
-    class Song
+   public class Song
     {
+        [Required]
+        public Guid SongID { get; set; }
+
+        [Required, MaxLength (100)]
+        public string Title { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Artist { get; set;  }
+
+        [MaxLength(250)]
+        public string FeaturedArtist { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Album { get; set; }
+
+        [MaxLength(100)]
+        public string Genre { get; set; }
+
+        [Required]
+        public DateTime ReleasedDate { get; set;  }
+
+
+        //Favorite referring to this song
+        public List<Favourite> FavouritesList { get; set; }
+
     }
 }
