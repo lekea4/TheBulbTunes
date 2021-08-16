50d496c37a418c231b02731c129c3a1bf4b35ce1
Adeleke Ayinde <lekea4@gmail.com>
2021-08-16 18:05:43 +0100

    Implement migration and upload the database

---
+++ TheBulbTunes/Migrations/20210816165729_InitialCreate.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheBulbTunes.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    SongID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Artist = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    FeaturedArtist = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    Album = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    ReleasedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.SongID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Favourites",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SelectedSongID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AddedByID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateAdded = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favourites", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Favourites_Songs_SelectedSongID",
+                        column: x => x.SelectedSongID,
+                        principalTable: "Songs",
+                        principalColumn: "SongID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Favourites_Users_AddedByID",
+                        column: x => x.AddedByID,
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favourites_AddedByID",
+                table: "Favourites",
+                column: "AddedByID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favourites_SelectedSongID",
+                table: "Favourites",
+                column: "SelectedSongID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Favourites");
+
+            migrationBuilder.DropTable(
+                name: "Songs");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
