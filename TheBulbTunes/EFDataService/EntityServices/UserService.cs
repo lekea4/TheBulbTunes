07c1f38321a2066b54dfba51adf76a0bf5a8e566
Adeleke Ayinde <lekea4@gmail.com>
2021-08-18 22:32:12 +0100

    Implement the create and FetchAll method of the UserService

---
+++ TheBulbTunes/EFDataService/EntityServices/UserService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheBulbTunes.EFDataService.Entities;
+
+namespace TheBulbTunes.EFDataService.EntityServices
+{
+    public class UserService
+    {
+        private readonly TheBulbTunesDBContext _contex = new TheBulbTunesDBContext();
+
+        private List<User> _users;
+
+        //Create a User 
+
+        public void Create(string firstName, string lastName, string emailAddress)
+        {
+            User newUser = new User
+            {
+                UserID = new Guid(),
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = emailAddress
+            };
+            _contex.Users.Add(newUser);
+            _contex.SaveChanges(); 
+        }
+
+        public List<User> FetchAll()
+        {
+            return _contex.Users.ToList();
+        }
+
+    }
+}
