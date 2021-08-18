bfb2751993d7e742e8a45ed482f57aa746fbbabd
Adeleke Ayinde <lekea4@gmail.com>
2021-08-18 14:17:05 +0100

    Implement the FetchWithFilter method

--- TheBulbTunes/EFDataService/EntityServices/SongService.cs
+++ TheBulbTunes/EFDataService/EntityServices/SongService.cs
@@ -12,7 +12,7 @@ namespace TheBulbTunes.EFDataService.EntityServices
 
         private readonly TheBulbTunesDBContext _context = new TheBulbTunesDBContext();
 
-        private List<Song> songs = new List<Song> { };
+        private List<Song> _songs;
 
         public SongService()
         {
@@ -41,5 +41,50 @@ namespace TheBulbTunes.EFDataService.EntityServices
         {
             return _context.Songs.ToList();
         }
+
+        public List<Song> FetchWithFilter(string titleFilter = null, string artistFilter = null,string genreFilter = null,string albumFilter = null)
+        {
+
+            //Retrieve all available songs unfiltered 
+            _songs = FetchAll();
+
+            if (titleFilter != null)
+                _songs = SearchByTitle(titleFilter, _songs);
+
+            if (artistFilter != null)
+                _songs = SearchByArtist(artistFilter, _songs);
+
+            if (genreFilter != null)
+                _songs = SearchByGenre(genreFilter, _songs);
+
+            if (albumFilter != null)
+                _songs = SearchByAlbum(albumFilter, _songs);
+            
+            return _songs;
+        }
+
+        private List<Song> SearchByTitle( string searchValue, List<Song> songs)
+        {
+            return songs.Where(s => s.Title.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        private List<Song> SearchByArtist(string searchValue, List<Song> songs)
+        {
+            return songs.Where(s => s.Artist.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        private List<Song> SearchByGenre(string searchValue, List<Song> songs)
+        {
+            return songs.Where(s => s.Genre.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+        } 
+
+        private List<Song> SearchByAlbum(string searchValue, List<Song> songs)
+        {
+            return songs.Where(s => s.Album.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+      //Update Song
+
+       //Delete Song
     }
 }
